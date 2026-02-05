73ed6fbb8d11929cf49a33a60778f3c17e6aa374
Diego Cuasapaz <diego.cuasapaz.heredia@outlook.com>
2026-02-04 20:15:36 -0500

    feat: Implementar carga automática de todos los Shapefiles en batch_load.sh
    - Automatizar detección de archivos .shp en fnt/ y subdirectorios
    - Determinar SRID automáticamente (32717 para INEC2012/DST_CRC, 4326 para SHP)
    - Generar nombres de tabla estandarizados (ec_ecu_<nombre>)
    - Corregir rutas absolutas en config.sh usando BASH_SOURCE
    - Actualizar README.md con nueva funcionalidad automática
    - Todas las pruebas pasan exitosamente

--- data_ingestion/postgis_dpa/bin/batch_load.sh
+++ data_ingestion/postgis_dpa/bin/batch_load.sh
@@ -3,6 +3,7 @@
 # MODIFICACIONES:														            #
 # FECHA  		AUTOR     		DESCRIPCION MOTIVO						            #
 # 2026-02-04	Diego Cuasapaz	Crear script de carga por lotes para optimización #
+# 2026-02-04	Diego Cuasapaz	Automatizar detección de todos los Shapefiles #
 #####################################################################################
 
 PROCESO=BATCH_LOAD_DPA
@@ -14,23 +15,54 @@ VAL_DIA=`date '+%Y%m%d'`
 VAL_HORA=`date '+%H%M%S'`
 VAL_LOG=$VAL_RUTA_LOG/$PROCESO"_"$VAL_DIA"_"$VAL_HORA.log
 
-echo `date '+%Y-%m-%d %H:%M:%S'`" INFO: Iniciando carga por lotes" >>$VAL_LOG
+echo `date '+%Y-%m-%d %H:%M:%S'`" INFO: Iniciando carga automática de todos los Shapefiles" >>$VAL_LOG
 
-# Lista de cargas: tabla,archivo,srid,subdir
-CARGAS=(
-    "ec_ecu_prv,Prv,4326,INEC2012"
-    "ec_ecu_cnt,Cnt,4326,INEC2012"
-    # Agregar más líneas aquí
-)
+# Función para determinar SRID basado en subdirectorio
+get_srid() {
+    local subdir=$1
+    case $subdir in
+        *INEC2012*|*DST_CRC*)
+            echo "32717"  # UTM 17S para datos proyectados
+            ;;
+        *)
+            echo "4326"   # WGS84 por defecto
+            ;;
+    esac
+}
 
-for CARGA in "${CARGAS[@]}"; do
-    IFS=',' read -r TABLE FILE SRID SUBDIR <<< "$CARGA"
-    echo `date '+%Y-%m-%d %H:%M:%S'`" INFO: Cargando $TABLE desde $SUBDIR/$FILE.shp" >>$VAL_LOG
-    sh -x $VAL_RUTA/load_shape.sh $TABLE $FILE $SRID $SUBDIR
+# Función para determinar nombre de tabla
+get_table_name() {
+    local filename=$1
+    local subdir=$2
+    # Convertir a minúsculas y agregar prefijo
+    local base_name=$(echo "$filename" | tr '[:upper:]' '[:lower:]')
+    echo "ec_ecu_${base_name}"
+}
+
+# Encontrar todos los archivos .shp en fnt/
+FNT_DIR="$VAL_RUTA/../fnt"
+find "$FNT_DIR" -name "*.shp" -type f | while read -r shp_file; do
+    # Obtener información del archivo
+    REL_PATH=${shp_file#"$FNT_DIR/"}
+    FILE_NAME=$(basename "$shp_file" .shp)
+    SUBDIR=$(dirname "$REL_PATH")
+    
+    # Determinar parámetros
+    TABLE=$(get_table_name "$FILE_NAME" "$SUBDIR")
+    SRID=$(get_srid "$SUBDIR")
+    
+    echo `date '+%Y-%m-%d %H:%M:%S'`" INFO: Detectado $shp_file -> Tabla: $TABLE, SRID: $SRID" >>$VAL_LOG
+    
+    # Ejecutar carga
+    echo `date '+%Y-%m-%d %H:%M:%S'`" INFO: Cargando $TABLE desde $SUBDIR/$FILE_NAME.shp" >>$VAL_LOG
+    bash $VAL_RUTA/load_shape.sh $TABLE $FILE_NAME $SRID $SUBDIR
+    
     if [ $? -ne 0 ]; then
         echo `date '+%Y-%m-%d %H:%M:%S'`" ERROR: Fallo en carga de $TABLE" >>$VAL_LOG
         exit 1
     fi
+    
+    echo `date '+%Y-%m-%d %H:%M:%S'`" INFO: $TABLE cargado exitosamente" >>$VAL_LOG
 done
 
-echo `date '+%Y-%m-%d %H:%M:%S'`" INFO: Carga por lotes completada" >>$VAL_LOG
+echo `date '+%Y-%m-%d %H:%M:%S'`" INFO: Carga automática completada" >>$VAL_LOG
